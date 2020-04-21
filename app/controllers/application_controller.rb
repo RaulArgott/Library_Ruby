00fcfc1e3e18febebae531ed70f9a35c7b32a412
Raul Argott <raulargott@hotmail.com>
2020-04-21 10:49:01 -0500

    get_current_controller_active_sidevar[D[D[D[D[D[D[Don_sidevar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_controller
+  def current_controller (name)    
+    return  controller_name == name ? "nav-link  active" : "nav-link"
+  end
 end
