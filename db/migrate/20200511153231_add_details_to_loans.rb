5b8361dee49f42cb9aeaf160e35298c725bb360a
Cesar Rdz <cesarjames06@gmail.com>
2020-05-11 11:48:38 -0500

    noloandsdesc

--- db/migrate/20200511153231_add_details_to_loans.rb
+++
@@ -1,7 +0,0 @@
-class AddDetailsToLoans < ActiveRecord::Migration[5.1]
-  def change
-    add_column :loans, :pay, :boolean
-    add_column :loans, :fee, :decimal
-    add_column :loans, :days, :integer
-  end
-end
