e5ad9fe095b11bbc161e03b76da346b250efe4e3
Gerardo Montalvo <gerardomontbec@hotmail.com>
2020-05-12 18:01:21 -0500

    FINAL

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+  	@books = Book.all.size
+  	@users = User.all.size
+  	@tag = Tag.all.size
+  	@favourite = Favourite.all.size
   end
 end
