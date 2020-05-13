e3726797450b4c9dcc1392773a6c527b9e6e2c13
Gerardo Montalvo <gerardomontbec@hotmail.com>
2020-05-13 14:48:05 -0500

    olov

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
     :domain         => 'gmail.com',
     :enable_starttls_auto => true
     }
-    config.action_mailer.default_url_options = {host: "localhost:3000"}
+  config.action_mailer.default_url_options = {host: "localhost:3000"}
 
   config.action_mailer.perform_caching = false
 
